05b9b37eddd3328fe4cdceaba1dd40f655e07012
Anton Semjonov <anton@semjonov.de>
2016-06-09 07:16:40 +0200

    modify chmod/chown recursive
    use pipes with null-character delimiter which is MUCH faster

--- bash.aliases/chmod-recursive.sh
+++ bash.aliases/chmod-recursive.sh
@@ -1,5 +1,6 @@
 # chmod / chown recursive
 alias chownr='sudo chown -R'		# recursive chown
 alias chmodr='sudo chmod -R'		# recursive chmod
-chmod-files-recursive() { find . -type f -exec chmod "$1" {} \;; }
-chmod-dirs-recursive()  { find . -type d -exec chmod "$1" {} \;; }
+
+chmod-files-recursive() { find . -type f -print0 | xargs -0 chmod "$1"; }
+chmod-dirs-recursive()  { find . -type d -print0 | xargs -0 chmod "$1"; }
