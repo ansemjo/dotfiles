eecc7abafe35f4a08dbfac39ec027a463673df82
Anton Semjonov <anton@semjonov.de>
2016-07-01 13:55:21 +0200

    pacman updates: copy systems sync db first
    move less data over the wire by first copying the systems sync db

--- bash.aliases/upgrades-list.sh
+++ bash.aliases/upgrades-list.sh
@@ -8,7 +8,8 @@ if [ -x /usr/bin/apt ]; then
 elif [ -x /usr/bin/pacman ]; then
     function upgrades {
         tmp=$(mktemp --directory --tmpdir pacman-check-upgrades-XXXXXX)
-        echo "note: using temporary sync db at '${tmp:?}'"
+        echo "using temporary db at '${tmp:?}'"
+        cp -r "/var/lib/pacman/sync" "$tmp"
         ln -s "/var/lib/pacman/local" "$tmp"
         fakeroot pacman -Sy --dbpath "$tmp"
         echo "------"
