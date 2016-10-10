7f6d61740dba8beb95843de03a503721d70cd680
Anton Semjonov <anton@semjonov.de>
2016-10-10 22:07:41 +0200

    update upgrades script to use pacaur

--- bash.aliases/upgrades-list.sh
+++ bash.aliases/upgrades-list.sh
@@ -1,19 +1,40 @@
 # update and then show upgrades
-if [ -x /usr/bin/apt ]; then
-    function upgrades {
-        apt update
-        echo "------"
-        apt list --upgradable
-    }
-elif [ -x /usr/bin/pacman ]; then
-    function upgrades {
-        tmp=$(mktemp --directory --tmpdir pacman-check-upgrades-XXXXXX)
-        echo "using temporary db at '${tmp:?}'"
-        cp -dR --preserve=all "/var/lib/pacman/sync" "$tmp"
-        ln -s "/var/lib/pacman/local" "$tmp"
-        fakeroot pacman -Sy --dbpath "$tmp"
-        echo "------"
-        pacman -Qu --dbpath "$tmp"
-        rm -rf "$tmp"
-    }
+
+if [[ -x /usr/bin/apt ]]; then
+  
+  # with apt, e.g. Debian, Ubuntu, ...
+  function upgrades {
+    apt update
+    echo "------"
+    apt list --upgradable
+  }
+
+elif [[ -x /usr/bin/pacman ]]; then
+  
+  # with pacman / pacaur, e.g. Arch ...
+  function upgrades {
+
+    # create tmp copy with user permissions
+    tmp=$(mktemp --directory --tmpdir pacman-check-upgrades-XXXXXX)
+    echo "using temporary db at '${tmp:?}'"
+    cp -dR --preserve=all "/var/lib/pacman/sync" "$tmp"
+    ln -s "/var/lib/pacman/local" "$tmp"
+
+    # update db
+    fakeroot pacman -Sy --dbpath "$tmp"
+
+    # if pacaur is installed, display updates with it
+    echo "------ Package updates:"
+    if [[ -x /usr/bin/pacaur ]]; then
+      pacaur -Qu -- --dbpath "$tmp"
+      echo "------ AUR package updates:"
+      pacaur -k
+    else
+      pacman -Qu --dbpath "$tmp"
+    fi
+
+    # cleanup temporary db
+    rm -rf "$tmp"
+  }
+
 fi
