14fb6cb4646dcb1f7f1912761bb9f9f37e04b5f8
Anton Semjonov <anton@semjonov.de>
2017-10-10 23:26:49 +0200

    edit weather alias to use https and allow param

--- bash/aliases.d/weather.sh
+++ bash/aliases.d/weather.sh
@@ -1,2 +1,2 @@
-# weather forecast
-alias weather='curl wttr.in'
+# weather forecast, allow insecure (their ssl cert is selfsigned and it's better than nothing)
+weather() { curl --insecure "https://wttr.in/${1}"; }
