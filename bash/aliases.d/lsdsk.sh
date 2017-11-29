b799ba7168dc36a8f7d97df5c4bfb042547bacc0
Anton Semjonov <anton@semjonov.de>
2017-11-29 11:22:45 +0100

    change lsdsk alias a little

--- bash/aliases.d/lsdsk.sh
+++ bash/aliases.d/lsdsk.sh
@@ -1,4 +1,4 @@
 # list all disks with size and model
 if iscommand lsblk; then
-  alias lsdsk='lsblk -dpo NAME,MODEL,SIZE'
+  alias lsdsk='lsblk --nodeps --paths --output NAME,SIZE,VENDOR,MODEL,SERIAL,TYPE,TRAN --sort MODEL';
 fi
