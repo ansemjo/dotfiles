ac8337bd32f83d64c0d527ce6b560321771f8ef5
Anton Semjonov <anton@semjonov.de>
2017-12-22 01:33:27 +0100

    add -nosalt usage info for openssl pipe

--- bash/aliases.d/openssl-pipe.sh
+++ bash/aliases.d/openssl-pipe.sh
@@ -24,23 +24,35 @@ decrypt() {
 #
 # Sample usage:
 #
-# $ encrypt rand <document.txt >ciphertext.aes
+# $ encrypt rand <document >ciphertext
 # 3Hrb9NyAPGvL4bCBZI5iKLo7JDTJuam0+6GpSiyHyPg=
 #
-# $ decrypt <ciphertext.aes
+# $ decrypt <ciphertext
 # enter aes-256-ctr decryption password:
 #
 # Alternatives:
 #
 # // Use a password file:
-# $ encrypt <document.txt >ciphertext.aes 2>pwfile.txt
-# $ decrypt <ciphertext.aes -pass file:pwfile.txt
+# $ encrypt <document >ciphertext 2>pwfile
+# $ decrypt <ciphertext -pass file:pwfile
 #
 # // Use -in/-out parameters:
-# $ encrypt rand -in document.txt -out ciphertext.aes
+# $ encrypt rand -in document -out ciphertext
 #
 # // Prompt for encryption password by omitting 'rand':
-# $ encrypt <document.txt >ciphertext.aes
+# $ encrypt <document >ciphertext
 # enter aes-256-ctr encryption password:
 # Verifying - enter aes-256-ctr encryption password:
 #
+# // Use no salt. Resulting file should look completely random:
+# // You should NOT use this with a prompted password!
+# $ encrypt <document rand -nosalt >ciphertext
+# 6HXjLgrneZCqlTbGMk6cQwP9KNmzorQhCA0bnM74v44=
+# $ xxd ciphertext
+# 00000000: f837 9708 22ea e98e ca1e 2da1 53bd 413a  .7..".....-.S.A:
+# 00000010: 5430 000a 461e 63b1 f5d7 6353 2048 f145  T0..F.c...cS H.E
+# ...
+# $ decrypt <ciphertext -nosalt
+# enter aes-256-ctr decryption password:
+#
+
