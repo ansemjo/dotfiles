3f90af3a19ce54c433e078ec10cbe3dd8370a4bf
Anton Semjonov <anton@semjonov.de>
2018-04-05 19:29:00 +0200

    bash: older versions of lsblk do not know '--sort'

--- bash/aliases.d/lsdsk.sh
+++ bash/aliases.d/lsdsk.sh
@@ -1,4 +1,4 @@
 # list all disks with size and model
 if iscommand lsblk; then
-  alias lsdsk='lsblk --nodeps --paths --output NAME,SIZE,VENDOR,MODEL,SERIAL,TYPE,TRAN --sort MODEL';
+  alias lsdsk='lsblk --nodeps --paths --output NAME,SIZE,VENDOR,MODEL,SERIAL,TYPE,TRAN';
 fi
