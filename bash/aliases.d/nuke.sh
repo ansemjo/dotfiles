0dc879dbf73f8a76c6e4091b6a56c6fbd915eefb
Anton Semjonov <anton@semjonov.de>
2018-04-10 21:45:54 +0200

    bash: add a nuke command
    nuke will recursively delete all files in a directory with shred and
    unlink them

---
+++ bash/aliases.d/nuke.sh
@@ -0,0 +1,19 @@
+# recursively shred files in given folder and then
+# remove directories with rm -rf
+
+if iscommand shred; then
+  nuke() {
+    if [[ -z $1 || ! -d $1 ]]; then
+      echo "specify directory: '$ nuke directory'"
+      return 1
+    fi
+    local n=$(find "$1" -type f -print | wc -l)
+    read -n 1 -p "this will completely delete $n files. are you sure? (y/N) " confirm
+    echo
+    if [[ $confirm == 'y' ]]; then
+      find "$1" -type f -printf 'shred %p\n' -exec shred --remove=wipe {} \; && rm -rvf "$1";
+    else
+      return 1
+    fi
+  }
+fi
