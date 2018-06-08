367e4ec6583648faa811a2ce8df9cf432a175799
Anton Semjonov <anton@semjonov.de>
2018-06-08 02:02:40 +0200

    add bash function to clean ssh multiplexing sockets
    including bash completion

---
+++ bash/aliases.d/ssh-muxclean.sh
@@ -0,0 +1,37 @@
+# provide a simple function to clean up ssh multiplexing sockets
+# for usage with ssh config 'ControlPath /run/user/%i/sshmux-%r@%h:%p.sock'
+SSHMUX_GLOB="/run/user/$EUID/sshmux-*.sock"
+muxclean () {
+  local g="$SSHMUX_GLOB"
+  if [[ -z $1 ]]; then
+    echo "\$ $0 (all / user@host:port)" >&2 && return 1
+  elif [[ $1 == all ]]; then
+    rm -Iv $g
+  else
+    mux="${g%%\**}$1${g##*\*}"
+    if [[ -S $mux ]]; then
+      rm -fv "$mux"
+    else
+      echo "no such mux: $mux" >&2 && return 1
+    fi
+  fi
+}
+
+_muxlist () {
+  local g="$SSHMUX_GLOB"
+  for f in $g; do
+    [[ $f != $g && $f =~ ${g%%\**}(.*)${g##*\*} ]] \
+      && echo "${BASH_REMATCH[1]}"
+  done
+}
+
+_muxclean () {
+  local cur prev words cword
+  _init_completion || return
+  [[ ${#words[@]} -gt 2 ]] && return
+  local list=$(_muxlist)
+  [[ -z $list ]] && echo "no ssh muxes found"
+  COMPREPLY=($(compgen -W "$list" "$cur"))
+}
+
+complete -F _muxclean muxclean
