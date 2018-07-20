ecb865a346ceec4dfc987c47b2ab5f6d56a8b512
Anton Semjonov <anton@semjonov.de>
2018-07-20 21:25:02 +0200

    bash: make mkcdtmp et al use diceware words for more memorable paths

--- bash/aliases.d/mkdir-and-enter.sh
+++ bash/aliases.d/mkdir-and-enter.sh
@@ -1,8 +1,18 @@
+# make temporary directory with diceware words
+mkdicetemp() { mktemp --tmpdir -d "tmp-$(words 2 -)-XXX"; }
+
 # make and enter
-mkcd () { ([[ -d ${1:?directory name required} ]] && echo "$1 already exists" >&2 || mkdir -p "$1") && cd "$1"; }
+mkcd() { ([[ -d ${1:?directory name required} ]] && echo "$1 already exists" >&2 || mkdir -p "$1") && cd "$1"; }
 
 # make and enter a temporary directory
-mkcdtmp () { cd $(mktemp -d) && pwd; }
+mkcdtmp() { cd $(mkdicetemp) && pwd; }
 
-# switch to completely temporary directory
-tmp () { t=$(mktemp -d); test -n "$t" && pushd "$t" && bash && popd && rm -rf "$t"; }
+# switch to ephemeral directory
+tmp() {
+  t=$(mkdicetemp) \
+    && pushd "$t" \
+    && echo 'this directory will be removed upon exit' \
+    && ${SHELL:-/usr/bin/env bash} \
+    && popd \
+    && rm -rf "$t"
+}
