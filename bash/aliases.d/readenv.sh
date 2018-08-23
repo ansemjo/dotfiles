ffee0074730583fbc2e02e2b9186201fbfec2734
Anton Semjonov <anton@semjonov.de>
2018-08-23 08:45:12 +0200

    bash: add function to read env from KEY=value files

---
+++ bash/aliases.d/readenv.sh
@@ -0,0 +1,7 @@
+# read environment from a simple KEY=value file without export
+# commands, like it is commonly used for service environment files
+readenv() {
+  set -o allexport
+  source "$1"
+  set +o allexport
+}
