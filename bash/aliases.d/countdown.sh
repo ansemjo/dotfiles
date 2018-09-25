3f9c05368d59cb592239815fb6fac01a670a0de7
Anton Semjonov <anton@semjonov.de>
2018-09-25 20:32:21 +0200

    bash: add ticks to show correct remainig amount in countdown

--- bash/aliases.d/countdown.sh
+++ bash/aliases.d/countdown.sh
@@ -10,7 +10,7 @@ countdown() {
   pattern_len=${#pattern[@]}
 
   while [[ $ticks -gt 0 ]]; do
-    printf '  %s [%02d] %s\r' "$message" "$(( $ticks / 10 ))" "${pattern[$i]}";
+    printf '  %s [%02d] %s\r' "$message" "$(( ($ticks+9) / 10 ))" "${pattern[$i]}";
     i=$(( (i+1) % ${#pattern[@]} ))
     ticks=$(( ticks - 1 ))
     sleep "0.098" # compensate a little
