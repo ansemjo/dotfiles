18b30c2ed106197299199ebe1711db694392928f
Anton Semjonov <anton@semjonov.de>
2018-09-27 16:55:07 +0200

    bash: add sed function to pop first line

--- bash/aliases.d/sed-magic.sh
+++ bash/aliases.d/sed-magic.sh
@@ -1,3 +1,6 @@
+# pop first line of a file
+popline() { sed -e "1w /dev/stdout" -e "1d" -i "${1:?file to pop}"; }
+
 # strip ansi escape codes from text
 # will only work with GNU sed
 # https://stackoverflow.com/a/43627833
