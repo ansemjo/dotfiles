f609089bdfef250b1368328844b0b5b1345c417e
Anton Semjonov <anton@semjonov.de>
2018-11-19 19:14:53 +0100

    bash: add fsnoise alias to create random files for testing purposes

---
+++ bash/aliases.d/fsnoise.sh
@@ -0,0 +1,19 @@
+# create some "filesystem noise" in the current directory by
+# creating directories and files randomly
+fsnoise() {
+  D=$1; F=$2; R=${3:-30};
+  [[ -n $D || -n $F ]] || {
+    printf 'fsnoise {dirs} {files} [rand%%]\n' >&2
+    return 1
+  }
+  for d in $(eval echo {$D}); do
+    for f in $(eval echo {$F}); do
+      [[ $RANDOM -lt $(( 327 * $R )) ]] && {
+        echo $d/$f
+        mkdir -p $d
+        uuidgen > $d/$f
+      }
+    done
+  done
+  return 0
+}
