2705b5bfd65d0b60f8ff5890b2572dc24bd36941
Anton Semjonov <anton@semjonov.de>
2018-11-19 19:13:28 +0100

    bash: remove --no-desktop from nautilus here

--- bash/aliases.d/nautilus-here.sh
+++ bash/aliases.d/nautilus-here.sh
@@ -1,4 +1,4 @@
 # open nautilus here (if system is gnome)
 if [ -x /usr/bin/nautilus ]; then
-  alias here='nautilus -w --no-desktop `pwd` &> /dev/null &'
+  alias here='nautilus -w "$PWD" &>/dev/null &'
 fi
