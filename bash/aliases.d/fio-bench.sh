ed12bbb86c1866853a4e8ae5d59dcc5aae5c2f9c
Anton Semjonov <anton@semjonov.de>
2018-11-23 16:50:38 +0100

    bash: add fio-bench alias with some defaults

---
+++ bash/aliases.d/fio-bench.sh
@@ -0,0 +1,2 @@
+# a fio alias for commonly used quick benchmarks
+alias fio-bench='fio --name fio-bench --ioengine=libaio --bs=4k --numjobs=4 --runtime=60 --group_reporting --direct=1 --iodepth=32 --size=2G --rw=rw'
