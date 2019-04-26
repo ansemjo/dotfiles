8b92088aecb0d5314197b7b2a2cef069400a633c
Anton Semjonov <anton@semjonov.de>
2019-04-26 10:43:39 +0200

    bash: allow more args to docker-ssh-socket

--- bash/aliases.d/docker-socket.sh
+++ bash/aliases.d/docker-socket.sh
@@ -6,9 +6,10 @@ if iscommand docker; then
   # environment for local docker to use forwarded socket
   docker-ssh-socket() {
     connect=${1:?destination host or ssh alias required}
+    shift 1
     socket="$XDG_RUNTIME_DIR/docker-$connect.sock"
     rm -f "$socket" && \
-    ssh -fN -n -T -o ControlPath=none -L "$socket:/var/run/docker.sock" "$connect" && \
+    ssh -fN -n -T -o ControlPath=none -L "$socket:/var/run/docker.sock" "$connect" "$@" && \
     export DOCKER_HOST="unix://$socket"
   }
 
