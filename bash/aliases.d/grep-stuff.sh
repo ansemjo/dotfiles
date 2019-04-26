cc9ed46ab2c2ebd0ee8ae3538a12147a45732023
Anton Semjonov <anton@semjonov.de>
2019-04-26 10:42:53 +0200

    bash: edit psgrep to not match itself

--- bash/aliases.d/grep-stuff.sh
+++ bash/aliases.d/grep-stuff.sh
@@ -2,6 +2,8 @@
 alias igrep='egrep -i'
 alias hh='history | igrep '
 alias ff='find . | igrep '
-alias psgrep='ps aux | igrep '
 alias lgrep='l | igrep '
 alias dpkgrep='dpkg --list | igrep '
+
+# grep for processes
+psgrep() { q="$1"; ps aux | grep -Ei "${q:0:1}.{0}${q:1}"; }
