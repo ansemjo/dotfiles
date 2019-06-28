709c47d9ea5186dfdcfed6123cc62409121c4a0d
Anton Semjonov <anton@semjonov.de>
2019-06-28 20:36:05 +0200

    bash: add randomname from docker name generator and switch tmp() to it

--- bash/aliases.d/mkdir-and-enter.sh
+++ bash/aliases.d/mkdir-and-enter.sh
@@ -4,12 +4,21 @@ mkdicetemp() { mktemp --tmpdir -d "tmp-$(words 2 -)-XXX"; }
 # make and enter
 mkcd() { ([[ -d ${1:?directory name required} ]] && echo "$1 already exists" >&2 || mkdir -p "$1") && cd "$1"; }
 
+# make temporary directory with docker name generator
+mknamedtemp() {
+  tmp=${TMPDIR:-/tmp}
+  while true; do
+    dir="$tmp/tmp-$(randomname)"
+    if mkdir "$dir"; then echo "$dir"; return 0; fi
+  done
+}
+
 # make and enter a temporary directory
-mkcdtmp() { cd $(mkdicetemp) && pwd; }
+mkcdtmp() { cd $(mknamedtemp) && pwd; }
 
 # switch to ephemeral directory
 tmp() {
-  t=$(mkdicetemp) \
+  t=$(mknamedtemp) \
     && { $SHELL -c \
      "cd '$t' \
       && printf '\033[31m%s\033[0m\n' 'this directory will be removed upon exit' \
