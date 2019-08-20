58018a5d600759b2d5481ee296f29b18fe182942
Anton Semjonov <anton@semjonov.de>
2019-08-20 19:45:27 +0200

    bash: replace recursive chmods with chmodr and argument parsing

--- bash/aliases.d/chmod-recursive.sh
+++ bash/aliases.d/chmod-recursive.sh
@@ -1,3 +1,34 @@
-# chmod all files/directories recursively
-chmod-recursive-files()       { find "${2:-.}" -type f -print0 | xargs -0 chmod "$1"; }
-chmod-recursive-directories() { find "${2:-.}" -type d -print0 | xargs -0 chmod "$1"; }
+# chmod files or directories recursively
+chmodr() {
+
+  # defaults
+  local TYPE="all"
+  local VEBOSE=""
+  
+  # usage note and error
+  usage() { echo "$ chmodr [-f | -d | -t type] [-v] mode directory"; }
+  err() { echo "err: $1" >&2; usage >&2; }
+
+  # argument parser
+  local OPTIND=0
+  while getopts 'dft:vh' opt; do
+    case $opt in
+      d) local TYPE="d" ;;
+      f) local TYPE="f" ;;
+      t) local TYPE="$OPTARG" ;;
+      v) local VERBOSE="-v" ;;
+      h) usage; return 0 ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  # mode and directory must be present
+  [[ -z $1 ]] && { err "mode must be given"; return 1; }
+  [[ -z $2 ]] && { err "directory must be given"; return 1; }
+  local MODE="${1:?mode required}"
+  shift 1
+
+  # find and chmod
+  find "$@" $([[ $TYPE != all ]] && printf -- '-type %q' "$TYPE") -print0 | xargs -r -0 chmod $VERBOSE "$MODE"
+
+}
