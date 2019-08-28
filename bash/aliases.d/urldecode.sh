6f08b946eb879a19219d4de1c60b72d644ed3e22
Anton Semjonov <anton@semjonov.de>
2019-08-28 08:46:09 +0200

    bash: add urlencode to the decode function in node

--- bash/aliases.d/urldecode.sh
+++ bash/aliases.d/urldecode.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-# use nodejs to urldecode a string
+# use nodejs to url-encode/decode a string
 if iscommand node; then
   urldecode() {
     node -e "process.stdin.resume(); process.stdin.setEncoding('utf8'); process.stdin.on('data', function(chunk) { process.stdout.write(decodeURIComponent(chunk)); });"
+  }
+  urlencode() {
+    node -e "process.stdin.resume(); process.stdin.setEncoding('utf8'); process.stdin.on('data', function(chunk) { process.stdout.write(encodeURIComponent(chunk)); });"
   }
 fi
