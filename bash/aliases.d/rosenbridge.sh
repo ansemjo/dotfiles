14377709eac2f504ef64036343633e21548a4377
Anton Semjonov <anton@semjonov.de>
2019-08-31 11:45:07 +0200

    bash: split rosenbridge to two seperate commands: rb / rbsend

--- bash/aliases.d/rosenbridge.sh
+++ bash/aliases.d/rosenbridge.sh
@@ -1,90 +1,62 @@
 #!/usr/bin/env bash
 
-# pipe stuff over a forwarding port on a multiplexed ssh connection
+# receive data over a forwarding port on a multiplexed ssh connection
 # this is basically my older tarpipe function, but without the implicit tar
 rosenbridge() {
 
 usage() { cat <<README
-$ $(basename "$0") {s|send} <port>      send input to forwarded port
-$ $(basename "$0") {r|recv} <sshconn>   receive input from ssh connection
+$ $(basename "$0") [ssh args] host
+Receive data over a forwarded port on a multiplexed ssh connection.
 README
 }
 
-  # two arguments required
-  if [[ -z $1 ]] || [[ -z $2 ]]; then
-    usage >&2; return 1;
+  # at least one argument required
+  [[ -z $1 ]] && { usage >&2; return 1; }
+
+  # create a new fifo
+  fifo=$(mktemp -p "${TMPDIR:-/tmp}" -u rosenbridge-XXXXXX)
+  mkfifo "$fifo"
+  # check if mux master is running or (re)start
+  if ! ssh -O check "$@" 2>/dev/null; then
+    ssh -fN -M "$@" || { echo "can't open control master" >&2; return 1; }
   fi
-
-  # modeselektor
-  case "$1" in
-
-    # receiver
-    r|recv|receive)
-      # create a new fifo
-      fifo=$(mktemp -p "${TMPDIR:-/tmp}" -u rosenbridge-XXXXXX)
-      mkfifo "$fifo"
-      # check if mux master is running or (re)start
-      if ! ssh -O check "$2" 2>/dev/null; then
-        ssh -fN -M "$2" || { echo "can't open control master" >&2; return 1; }
-      fi
-      # add forwarding in multiplexed connection
-      remote=$(ssh -O forward -R "0:$fifo" "$2") || \
-        { echo "can't create forwarding" >&2; return 1; }
-      printf 'Listening on port \033[1m%s\033[0m remotely ...\n' "$remote" >&2
-      # listen with netcat
-      nc -Ul "$fifo"
-      # cancel forwarding and remove fifo when done
-      # TODO: couldn't get traps for cleanup to work reliably so far ..
-      ssh -O cancel -R "0:$fifo" "$2" 2>/dev/null
-      rm -f "$fifo"
-    ;;
-
-    # sender
-    s|send)
-      # just pipe to given port with bash
-      cat >/dev/tcp/localhost/"$2"
-    ;;
-
-    # unknown
-    *) usage >&2; return 1;;
-
-  esac
+  # add forwarding in multiplexed connection
+  remote=$(ssh -O forward -R "0:$fifo" "$@") || \
+    { echo "can't create forwarding" >&2; return 1; }
+  printf 'Listening on port \033[1m%s\033[0m remotely ...\n' "$remote" >&2
+  # listen with netcat
+  nc -Ul "$fifo"
+  # cancel forwarding and remove fifo when done
+  # TODO: couldn't get traps for cleanup to work reliably so far ..
+  ssh -O cancel -R "0:$fifo" "$@" 2>/dev/null
+  rm -f "$fifo"
 
 }
-
 _rosenbridge() {
-  local cur prev words cword
-  _init_completion || return
-
-  # first argument is the command
-  if [[ $cword -eq 1 ]]; then
-    COMPREPLY=($(compgen -W "send receive" -- "$cur"))
-    return
+  if iscommand _ssh; then
+    _ssh
+  else
+    local cur prev words cword
+    _init_completion || return
+    _known_hosts_real -a -- "$cur"
   fi
-
-  # second argument
-  if [[ $cword -eq 2 ]]; then
-    # switch on command
-    case "$prev" in
-      
-      s*) # sending -> listening ports
-        local ports
-        ports=$(ss -tlpn | sed -n 's/.*:\([0-9]\{5\}\) .*/\1/p' | sort | uniq)
-        COMPREPLY=($(compgen -W "$ports" -- "$cur"))
-        return
-      ;;
-
-      r*) # receive -> ssh known hosts
-        _known_hosts_real -a -- "$cur"
-        return
-      ;;
-
-    esac
-  fi
-  COMPREPLY=()
 }
 complete -F _rosenbridge rosenbridge
 
+# simply pipe to a given local port, send data to rosenbridge
+rbsend() {
+  [[ -z $1 ]] && { echo -e "err: port required\n$ rbsend <port>" >&2; return 1; };
+  cat >/dev/tcp/localhost/"${1:?port required}";
+}
+_rbsend() {
+  local cur prev words cword  ports
+  _init_completion || return
+  ports=$(ss -tlpn | sed -n 's/.*:\([0-9]\{5\}\) .*/\1/p' | sort | uniq)
+  COMPREPLY=($(compgen -W "$ports" -- "$cur"))
+}
+complete -F _rbsend rbsend
+
+# add shorter alias if free
 if ! iscommand rb; then
   alias rb=rosenbridge
   complete -F _rosenbridge rb
