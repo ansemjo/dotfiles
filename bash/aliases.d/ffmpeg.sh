b6740b401364f0b040b762db2012153858fb8107
Anton Semjonov <anton@semjonov.de>
2019-09-05 23:39:24 +0200

    bash: improve ffmpeg-concat with a readlink invocation

--- bash/aliases.d/ffmpeg.sh
+++ bash/aliases.d/ffmpeg.sh
@@ -1,7 +1,11 @@
 # concatenate video files with ffmpeg
 # expects absolute filenames on stdin, one per line
 if iscommand ffmpeg; then
-ffmpeg-concat() {
-  ffmpeg -f concat -safe 0 -i <(sed "s/^/file '/; s/$/'/") -c copy "$@";
-}
+  ffmpeg-concat() {
+    ffmpeg -hide_banner -f concat -safe 0 \
+      -i <(while read line; do \
+        f=$(readlink -f "$line"); \
+        printf "file '%s'\n" "${f/\'/\'}"; \
+      done) "$@";
+  }
 fi
