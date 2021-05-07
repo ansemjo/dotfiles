91ca02d9f2b78c76bb062534be715509b3407dee
Anton Semjonov <anton@semjonov.de>
2021-05-08 00:31:20 +0200

    bash: use signify to create signed checkusm in sqp

--- bash/aliases.d/squashpipe.sh
+++ bash/aliases.d/squashpipe.sh
@@ -6,34 +6,37 @@ squashpipe() {
 
   err() { echo "err: $1" >&2; usage; return 1; }
   usage() { cat >&2 <<USAGE
-usage: $ squashpipe [-n name] [-m mode] [-c] archive.sqsh [extra]
+usage: $ squashpipe [-n name] [-m mode] [-s seckey] archive.sqsh [extra]
   -n name    filename in archive
   -m mode    octal mode of file
-  -c         create checksum file on-the-fly
+  -s seckey  create signed checksum with signify on-the-fly
   extra      extra arguments passed to mksquashfs
 USAGE
 }
 
   # defaults
   local ARCHIVE
-  local NAME=image.bin
+  local RAWNAME NAME=image.bin
   local MODE=644
-  local CHECKSUM=no
+  local SIGN=no
+  local SECKEY
 
   # parse commandline
   local OPT OPTIND
-  while getopts ":n:m:ch" OPT; do
+  while getopts ":n:m:s:h" OPT; do
     case "$OPT" in
 
       n) # filename
+        RAWNAME="$OPTARG"
         NAME=$(sed 's/[ \\]/\\&/g' <<<"$OPTARG") ;;
       
       m) # octal mode
         [[ $OPTARG =~ ^[0-7]{3}$ ]] || { err "mode must be octal"; }
         MODE="$OPTARG" ;;
 
-      c) # create checksum
-        CHECKSUM=yes ;;
+      s) # create signature
+        SECKEY="$OPTARG"
+        SIGN=yes ;;
 
       # help, invalid, etc.
       h) usage; return 1 ;;
@@ -48,22 +51,27 @@ USAGE
   [[ $# -eq 0 ]] && { err "archive filename is required!"; }
   ARCHIVE="$1"; shift 1;
 
+  # run in subshell
+  ( set -eo pipefail
+  
   # create an empty dummy directory, so there is no other file in archive
-  tmp=$(mktemp -d --tmpdir squashpipe-tmp-XXXXXXXXX)
-  mkdir -p "$tmp/empty"
-  trap "rm -rf ${tmp@Q}" RETURN
+  dummy=$(mktemp -d --tmpdir squashpipe-tmp-XXXXXXXXX)
+  mkdir -p "$dummy/empty"
+  trap "rm -rf ${dummy@Q}" RETURN
 
   # constructed mksquash command
-  sqsh() { mksquashfs "$tmp/empty" "$ARCHIVE" -all-root -quiet -p "$NAME f $MODE 0 0 cat" "$@"; }
+  sqsh() { mksquashfs "$dummy/empty" "$ARCHIVE" -all-root -quiet -p "$NAME f $MODE 0 0 cat" "$@"; }
 
   # let's roll
-  if [[ $CHECKSUM = yes ]]; then
-    [[ $NAME = SHA256SUM ]] && { err "filename conflicts with checksum"; }
-    tee >(sha256sum | head -c66 >"$tmp/SHA256SUM") | sqsh "$@"
-    echo "$NAME" >>"$tmp/SHA256SUM"
-    mksquashfs "$tmp/SHA256SUM" "$ARCHIVE" -all-root -quiet -no-progress >/dev/null
+  if [[ $SIGN = yes ]]; then
+    tee >(sha256sum --tag | awk -v "name=$RAWNAME" '{ gsub("-", name, $2); print }' >"$dummy/checksum") | sqsh "$@"
+    mksquashfs "$dummy/checksum" "$ARCHIVE" -all-root -quiet -no-progress >/dev/null
+    echo "signing checksum ..." >&2
+    signify -S -e -m - -s "$SECKEY" -x "$dummy/checksum.sig" < "$dummy/checksum"
+    mksquashfs "$dummy/checksum.sig" "$ARCHIVE" -all-root -quiet -no-progress >/dev/null
   else
     sqsh "$@"
-  fi  
+  fi
+  )
 
 }
