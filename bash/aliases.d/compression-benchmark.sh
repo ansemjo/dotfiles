ansemjo/dotfiles

commit af849949ce084bc7a07227664b4c4cf0ab490847

bash: check aliases manually with shellcheck

bash/aliases.d/compression-benchmark.sh
@@ -15,14 +15,15 @@ fi
 
 shift 1;
 COMPRESSORS=("$@")
-if [[ -z $COMPRESSORS ]]; then
+if [[ ${#COMPRESSORS[@]} -eq 0 ]]; then
+  # use defaults if none given
   COMPRESSORS=(cat lz4 zstd gzip xz lzip)
 fi
 
 say() { printf '\033[1m%s\033[0m %s\n' "$1" "$2"; }
 
 # output original file info
-SIZE=$(wc -c <$FILE)
+SIZE=$(wc -c <"$FILE")
 say "Original file:" "$FILE, $SIZE bytes"
 
 for compressor in "${COMPRESSORS[@]}"; do
@@ -33,9 +34,9 @@ for compressor in "${COMPRESSORS[@]}"; do
   fi
 
   # runtime info in: $outputsize[Bytes] $runtime[seconds]
-  run=$( (TIMEFORMAT='%3R'; time $compressor <$FILE | wc -c) 2>&1)
-  o=$(echo $run | awk '{print $1}')
-  t=$(echo $run | awk '{print $2}')
+  run=$( (TIMEFORMAT='%3R'; time $compressor <"$FILE" | wc -c) 2>&1)
+  o=$(echo "$run" | awk '{print $1}')
+  t=$(echo "$run" | awk '{print $2}')
 
   # calculate compression ratio
   ratio=$(bc -l <<< "scale=5; $o / $SIZE")

bash/aliases.d/anykey.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # wait for any keypress
-anykey () { read -rsp $'Press any key to continue...\n' -n1 key; }
+anykey () { read -rsp $'Press any key to continue...\n' -n1 _; }

bash/aliases.d/archlinux.sh
@@ -4,15 +4,18 @@
 if [[ -x /usr/bin/rankmirrors ]]; then
   update-mirrorlist() {
 
+    set -e
     say() { printf '%s\n' "$*" >&2; };
 
     # https://wiki.archlinux.org/index.php/Mirrors#Fetching_and_ranking_a_live_mirror_list
     say "fetch updated mirrorlist: DE/https/status=on ..."
-    local mirrors=$(curl -sL "https://archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" |\
+    local mirrors
+    mirrors=$(curl -sL "https://archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" |\
       sed -e 's/^#Server/Server/' -e '/^#/d')
 
     say "rank mirrors ..."
-    local ranked=$(rankmirrors -n 10 - <<<"$mirrors")
+    local ranked
+    ranked=$(rankmirrors -n 10 - <<<"$mirrors")
 
     local list="/etc/pacman.d/mirrorlist"
     say "replace $list ..."
@@ -26,8 +29,11 @@ archnews() {
 # Credit belongs here:
 # https://bbs.archlinux.org/viewtopic.php?pid=1145058#p1145058
 
+    set -e
+
     # Set the feed
-    if [ ! -n "$1" ]; then
+    local feed
+    if [ -z "$1" ]; then
         # Arch Linux News by default
         feed="https://www.archlinux.org/feeds/news/";
     else
@@ -35,16 +41,17 @@ archnews() {
     fi
     
     # Get the feed
-    local rss_source="$(curl --silent $feed | sed -e ':a;N;$!ba;s/\n/ /g')";
+    local rss_source
+    rss_source="$(curl --silent "$feed" | sed -e ':a;N;$!ba;s/\n/ /g')";
     
     # Display the feed
-    if [ ! -n "$rss_source" ]; then
+    if [ -z "$rss_source" ]; then
         echo "The feed is empty";
         return 1;
     fi
     
     # The characters "£, §" are used as metacharacters. They should not be encountered in a feed...
-    echo -e "$(echo $rss_source | \
+    echo -e "$(echo "$rss_source" | \
     sed -e 's/&amp;/\&/g
     s/&lt;\|&#60;/</g
     s/&gt;\|&#62;/>/g

bash/aliases.d/chmod-recursive.sh
@@ -4,6 +4,7 @@ chmodr() {
 
   # defaults
   local TYPE="all"
+  # shellcheck disable=SC2034
   local VEBOSE=""
   
   # usage note and error
@@ -14,10 +15,10 @@ chmodr() {
   local opt OPTIND OPTARG
   while getopts 'dft:vh' opt; do
     case $opt in
-      d) local TYPE="d" ;;
-      f) local TYPE="f" ;;
-      t) local TYPE="$OPTARG" ;;
-      v) local VERBOSE="-v" ;;
+      d) TYPE="d" ;;
+      f) TYPE="f" ;;
+      t) TYPE="$OPTARG" ;;
+      v) VERBOSE="-v" ;;
       h) usage; return 0 ;;
       *) usage; return 1 ;;
     esac
@@ -31,6 +32,7 @@ chmodr() {
   shift 1
 
   # find and chmod
+  # shellcheck disable=SC2046
   find "$@" $([[ $TYPE != all ]] && printf -- '-type %q' "$TYPE") -print0 | xargs -r -0 chmod $VERBOSE "$MODE"
 
 }

bash/aliases.d/countdown.sh
@@ -5,17 +5,14 @@ countdown() {
 
   ticks=$(( ${1:-10} * 10 ))
   message=${2:-waiting}
-
   pattern=(⡇ ⠏ ⠛ ⠹ ⢸ ⣰ ⣤ ⣆)
-  pattern_len=${#pattern[@]}
 
   while [[ $ticks -gt 0 ]]; do
-    printf '  %s [%02d] %s\r' "$message" "$(( ($ticks+9) / 10 ))" "${pattern[$i]}";
+    printf '\033[2K\r  %s [%02d] %s\r' "$message" "$(( (ticks + 9) / 10 ))" "${pattern[$i]}";
     i=$(( (i+1) % ${#pattern[@]} ))
     ticks=$(( ticks - 1 ))
     sleep "0.098" # compensate a little
   done
-
-  printf '\rdone.                      \n';
+  printf '\033[2K\r'
 
 }

bash/aliases.d/curlsum.sh
@@ -8,6 +8,7 @@ curlsum() {
   # parse checksum arg (algorithm/checksum)
   alg=$(dirname "$checksum")
   KNOWNALG=(md5 sha1 sha224 sha256 sha384 sha512)
+  # shellcheck disable=SC2076
   if [[ -z $alg ]] || [[ ! " ${KNOWNALG[*]} " =~ " ${alg} " ]] || ! which "${alg}sum" >/dev/null; then
     echo "unknown checksum algorithm: '$alg'" >&2; return 1;
   fi

bash/aliases.d/docker-socket.sh
@@ -17,6 +17,7 @@ if iscommand docker; then
     if iscommand _ssh; then
       _ssh
     else
+      # shellcheck disable=SC2034
       local cur prev words cword
       _init_completion || return
       _known_hosts_real -a -- "$cur"

bash/aliases.d/extract.sh
@@ -27,6 +27,7 @@ if iscommand unzip; then
 unzplit() {
   file=${1:?input *.zip required}
   tmp=$(mktemp --tmpdir unzplit-XXXXX.zip)
+  # shellcheck disable=SC2064
   trap "rm -fv '${tmp:?tempfile}'" EXIT RETURN
   zip -s- "$file" --out "$tmp"
   unzip "$tmp"

commit 11f3de72098d332e45daf8ebf71535c3882ae3e4

bash: fix compression-benchmark broken by af84994

bash/aliases.d/compression-benchmark.sh
@@ -34,9 +34,9 @@ for compressor in "${COMPRESSORS[@]}"; do
   fi
 
   # runtime info in: $outputsize[Bytes] $runtime[seconds]
-  run=$( (TIMEFORMAT='%3R'; time $compressor <"$FILE" | wc -c) 2>&1)
-  o=$(echo "$run" | awk '{print $1}')
-  t=$(echo "$run" | awk '{print $2}')
+  run=($( (TIMEFORMAT='%3R'; time $compressor <"$FILE" | wc -c) 2>&1))
+  o=$(echo "${run[@]}" | awk '{print $1}')
+  t=$(echo "${run[@]}" | awk '{print $2}')
 
   # calculate compression ratio
   ratio=$(bc -l <<< "scale=5; $o / $SIZE")