25d57d3e942337ce1baf37558b3a513c1e6d60aa
Anton Semjonov <anton@semjonov.de>
2021-07-31 21:37:59 +0200

    bash: add send function for ffsend with basic-auth

---
+++ bash/aliases.d/ffsend.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# upload to a mozilla send host that is behin basic auth
+# see: https://gist.github.com/ansemjo/c24267ea4684a09dd841db910d908b9b
+if iscommand ffsend; then
+  function send() {
+    # store user:password with $ secret-tool store --label="..." ffsend $FFSEND_HOST
+    if [[ -z $FFSEND_HOST ]]; then
+      echo "err: this command requires a host in FFSEND_HOST and stored credentials" >&2
+      return 1
+    fi
+    ffsend upload \
+      --host "$FFSEND_HOST" \
+      --basic-auth "$(secret-tool lookup ffsend "$FFSEND_HOST")" \
+      "$@";
+  }
+fi
