30aaf8f2d7cd746eb743db8dc9e3e39f157e991b
Anton Semjonov <anton@semjonov.de>
2021-08-01 00:26:02 +0200

    bash: change the picocom completion, match all non-/ttyS?[0-9]+

--- bash/aliases.d/picocom.sh
+++ bash/aliases.d/picocom.sh
@@ -5,7 +5,8 @@
 if iscommand picocom; then
   alias picocom='picocom --baud 115200 --omap crcrlf,delbs --quiet'
   # autocompletion with a few common paths
-  _picocom_serials() { COMPREPLY=($(compgen -W "$(ls /dev/{tty{USB,My,ACM},serial/by-id/}* 2>/dev/null)" "${COMP_WORDS[1]}")); }
+  # shellcheck disable=SC2010
+  _picocom_serials() { COMPREPLY=($(compgen -W "$(ls /dev/{tty,serial/by-id/}* 2>/dev/null | grep -vE '/tty(S?[0-9]+)?$' 2>/dev/null)" "${COMP_WORDS[1]}")); }
   complete -F _picocom_serials picocom
 fi
 
