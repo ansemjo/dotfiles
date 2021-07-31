37126b9c1548e1fd40d79e03d90c28230c07a2f1
Anton Semjonov <anton@semjonov.de>
2021-08-01 00:00:03 +0200

    bash: add the GOPATH config itself

--- bash/conf.d/60-path-go.sh
+++ bash/conf.d/60-path-go.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# change GOPATH to tidy up homedir
+# could also be added to /etc/profile.d/gopath.sh
+export GOPATH=~/.local/go
+
 # add $GOPATH/bin to $PATH if go is installed
 if command -v go >/dev/null; then
   export PATH="${GOPATH:-$(go env GOPATH)}/bin:$PATH"
