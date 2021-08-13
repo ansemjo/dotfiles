3a35fe5c37c8b403423e27acf5c9802374fe903e
Anton Semjonov <anton@semjonov.de>
2021-08-13 12:38:19 +0200

    bash: improve picocom completion to handle some flags

--- bash/aliases.d/picocom.sh
+++ bash/aliases.d/picocom.sh
@@ -6,7 +6,10 @@ if iscommand picocom; then
   alias picocom='picocom --baud 115200 --omap crcrlf,delbs --quiet'
   # autocompletion with a few common paths
   # shellcheck disable=SC2010
-  _picocom_serials() { COMPREPLY=($(compgen -W "$(ls /dev/{tty,serial/by-id/}* 2>/dev/null | grep -vE '/tty(S?[0-9]+)?$' 2>/dev/null)" "${COMP_WORDS[1]}")); }
+  _picocom_serials() {
+    flags=(--{baud,flow,parity,{data,stop}bits,{no-,}escape,no{init,reset},{i,o,e}map,logfile,{lower,raise}-{rts,dtr},exit,quiet,help});
+    COMPREPLY=($(compgen -W "$(ls /dev/{tty,serial/by-id/}* 2>/dev/null | grep -vE '/tty(S?[0-9]+)?$' 2>/dev/null) ${flags[*]}" -- "${COMP_WORDS[-1]}"));
+  }
   complete -F _picocom_serials picocom
 fi
 
