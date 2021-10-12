ed4b20aadf08633d4a5d05e2ccf3c1026a787699
Anton Semjonov <anton@semjonov.de>
2021-10-12 15:30:20 +0200

    bash: change compression-benchmark output format to ascii table

--- bash/aliases.d/benchmarking.sh
+++ bash/aliases.d/benchmarking.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
-# check efficiency of various compressors on a file
+# check performance of various compressors on a file
 compression-benchmark() {
 
-  # parameters:
-  #  $1  : file which to compress
-  #  $2+ : list of compressors to use
-
-  FILE=$1
-  if [[ -z $FILE || ! -f $FILE ]]; then
-    printf 'no such / not a file: %q\n' "$FILE" >&2;
-    printf 'usage: $ %s testing-file [compressor [compressor]]\n' "${FUNCNAME[0]}" >&2;
+  # first argument is the file to test on
+  if [[ -z ${1+defined} ]] || ! [[ -f $1 ]] || ! [[ -r $1 ]]; then
+    printf 'ERR: readable file required as argument: %q\n' "$1" >&2;
+    printf 'usage: $ %s <file> [compressor [compressor]]\n' "${FUNCNAME[0]}" >&2;
     return 1;
   fi
+  FILE=$1; shift 1;
+  SIZE=$(wc -c <"$FILE")
 
-  shift 1;
+  # remaining arguments are the compressors
   COMPRESSORS=("$@")
   if [[ ${#COMPRESSORS[@]} -eq 0 ]]; then
     # use defaults if none given
-    COMPRESSORS=(cat lz4 zstd gzip xz lzip)
+    COMPRESSORS=(cat gzip zstd xz)
   fi
 
-  say() { printf '\033[1m%s\033[0m %s\n' "$1" "$2"; }
+  # print a header and function for result lines
+  printf "| %10s | %10s | %14s | %8s | %8s |\n" Compressor Time Bytes Ratio Score
+  printf "|------------|------------|----------------|----------|----------|\n"
+  res() { printf "\r| %10s | %10s | %14s | %8s | %8s |\n" "$@"; }
 
-  # output original file info
-  SIZE=$(wc -c <"$FILE")
-  say "Original file:" "$FILE, $SIZE bytes"
+  # print original file info
+  res original "" "$SIZE" "1.000" ""
 
+  # try all the compressors and print results
   for compressor in "${COMPRESSORS[@]}"; do
  
-    say "Testing compressor:" "$compressor";
-    if ! $compressor <<< 'Hello, World!' >/dev/null; then
-      continue
-    fi
+    # check if compressor works
+    if ! $compressor <<< 'Hello, World!' >/dev/null; then continue; fi
+    printf ' testing %s ...' "$compressor"
 
     # runtime info in: $outputsize[Bytes] $runtime[seconds]
     run=($( (TIMEFORMAT='%3R'; time $compressor <"$FILE" | wc -c) 2>&1))
@@ -40,17 +40,17 @@ compression-benchmark() {
     t=$(echo "${run[@]}" | awk '{print $2}')
 
     # calculate compression ratio
-    ratio=$(bc -l <<< "scale=5; $o / $SIZE")
+    ratio=$(bc -l <<< "scale=3; $o / $SIZE")
 
-    # calculate performance metric
+    # calculate an arbitrary performance metric
+    # TODO: find a better calculation?
     # plot: https://www.wolframalpha.com/input/?i=3d+plot+ln(-ln(c)%2Fln(t%2B1)%2B1)+from+c%3D0.1..1,+t%3D0..10
     score=$(bc -l <<< "scale=3; l((-l($ratio)/l($t+1))+1)" 2>/dev/null)
-    [[ -z $score ]] && score="∞";
+    #score=$(bc -l <<< "scale=3; (-l($ratio)/l($t+1))+1" 2>/dev/null)
+    if [[ -z $score ]]; then score="∞"; fi
   
-    echo " time   : $t seconds";
-    echo " output : $o Bytes";
-    echo " ratio  : $ratio";
-    echo " score  : $score";
+    # print result
+    res "$compressor" "$t" "$o" "$ratio" "$score"
 
   done
 
