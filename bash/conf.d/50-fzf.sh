32485eb7e9774b59e0d25e4c2b7b802fe4b7786b
Anton Semjonov <anton@semjonov.de>
2023-01-13 10:56:43 +0100

    bash: fix search for fzf keybindings

--- bash/conf.d/50-fzf.sh
+++ bash/conf.d/50-fzf.sh
@@ -3,14 +3,14 @@
 # add fzf keybindings and completion if it is installed
 if command -v fzf >/dev/null; then
 
-if [[ -d /usr/share/fzf ]]; then
-  source /usr/share/fzf/key-bindings.bash
-  source /usr/share/fzf/completion.bash
-fi
-
-if [[ -d /usr/share/doc/fzf/examples ]]; then
-  source /usr/share/doc/fzf/examples/key-bindings.bash
-  source /usr/share/doc/fzf/examples/completion.bash
-fi
+  for dir in /usr/share/fzf /usr/share/doc/fzf/examples; do
+    if [[ -d $dir ]]; then
+      for file in key-bindings.bash completion.bash; do
+        if [[ -r $dir/$file ]]; then
+          source "$dir/$file"
+        fi
+      done
+    fi
+  done
 
 fi
