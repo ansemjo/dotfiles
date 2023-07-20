104aaec799c64f33ec7299bab23b7e6b5c0512cc
Anton Semjonov <anton@semjonov.de>
2023-07-20 10:17:50 +0200

    bash: use bfs for fzf file search

--- bash/conf.d/50-fzf.sh
+++ bash/conf.d/50-fzf.sh
@@ -3,6 +3,12 @@
 # add fzf keybindings and completion if it is installed
 if command -v fzf >/dev/null; then
 
+  # use breadth-first search when available
+  if command -v bfs >/dev/null; then
+    export FZF_DEFAULT_COMMAND="bfs -s -nocolor 2>/dev/null"
+    export FZF_CTRL_T_COMMAND="bfs -s -nocolor 2>/dev/null"
+  fi
+
   for dir in /usr/share/fzf /usr/share/doc/fzf/examples; do
     if [[ -d $dir ]]; then
       for file in key-bindings.bash completion.bash; do
