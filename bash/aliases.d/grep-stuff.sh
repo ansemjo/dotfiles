cb765787fc5c3232f348a6ffc79d871dc86797d0
Anton Semjonov <anton@semjonov.de>
2023-07-20 10:00:39 +0200

    bash: update grep aliases and prefer fzf

--- bash/aliases.d/grep-stuff.sh
+++ bash/aliases.d/grep-stuff.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 # various grep shorthands
-alias igrep='egrep -i'
-alias hh='history | igrep '
-alias ff='find . | igrep '
-alias lgrep='l | igrep '
-alias dpkgrep='dpkg --list | igrep '
+# NOTE: these are much better served by fzf
+if iscommand fzf; then
+  alias hh='history | fzf --tac ' # or configure ctrl-R in bash
+  if iscommand bfs; then
+    alias ff='bfs | fzf ' # or configure ctrl-T
+  else
+    alias ff='find . | fzf '
+  fi
+  if iscommand dpkg; then
+    alias dpkgrep='dpkg --list | fzf '
+  fi
+else
+  alias hh='history | grep -iE '
+  alias ff='find . | grep -iE '
+  alias dpkgrep='dpkg --list | grep -iE '
+fi
 
 # grep for processes
 # shellcheck disable=2009
