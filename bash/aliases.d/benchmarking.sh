3d53e0998d98ec185e35dd597b5de1b7f9495d50
Anton Semjonov <anton@semjonov.de>
2024-02-20 12:35:42 +0100

    bash: use fio benchmark more akin to crystaldisk

--- bash/aliases.d/benchmarking.sh
+++ bash/aliases.d/benchmarking.sh
@@ -82,7 +82,17 @@ compression-benchmark() {
 if iscommand fio; then
 
   # a fio alias for commonly used quick benchmarks
-  alias fio-bench='fio --name fio-bench --ioengine=libaio --bs=4k --numjobs=4 --runtime=60 --group_reporting --direct=1 --iodepth=32 --size=2G --rw=rw'
+  #alias fio-bench='fio --name fio-bench --ioengine=libaio --bs=4k --numjobs=4 --runtime=60 --group_reporting --direct=1 --iodepth=32 --size=2G --rw=rw'
+  # https://unix.stackexchange.com/a/392091
+  fio-crystalbench() {
+    fio --loops=5 --size=1000m --filename=fiocrystalbench.tmp --stonewall --ioengine=libaio --direct=1 \
+      --name=Seqread --bs=1m --rw=read \
+      --name=Seqwrite --bs=1m --rw=write \
+      --name=512Kread --bs=512k --rw=randread \
+      --name=512Kwrite --bs=512k --rw=randwrite \
+      --name=4kQD32read --bs=4k --iodepth=32 --rw=randread \
+      --name=4kQD32write --bs=4k --iodepth=32 --rw=randwrite;
+  }
 
 fi
 
