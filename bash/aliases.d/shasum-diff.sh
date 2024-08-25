7ca0ae2218a2736a1a387412ef17a6020dc15251
Anton Semjonov <anton@semjonov.de>
2024-08-25 12:05:42 +0200

    bash: add shadupes to check for duplicate files by checksum

--- bash/aliases.d/shasum-diff.sh
+++ bash/aliases.d/shasum-diff.sh
@@ -5,3 +5,10 @@ shadiff() {
   (cd "${1:?first directory}" && find . -type f -exec "$shasum" {} \;) | \
     (cd "${2:?second directory}" && "$shasum" -c -)
 }
+
+# check for duplicate files by calculating checksum and printing duplicates
+shadupes() {
+  if [[ -n $1 ]]; then files=("$@"); else files=(*); fi
+  echo "checking ${#files[@]} files with $(du -shc "${files[@]}" | tail -1) .."
+  sha256sum "${files[@]}" | sort | uniq -w64 --all-repeated=prepend | cut -b 67-;
+}
