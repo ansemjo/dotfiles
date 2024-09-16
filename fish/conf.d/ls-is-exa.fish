7ad5d7ef7ce70615b7967d238b887c64a9f78ed6
Anton Semjonov <anton@semjonov.de>
2024-09-16 08:19:52 +0000

    fish: older exa takes no args after --classify

--- fish/conf.d/ls-is-exa.fish
+++ fish/conf.d/ls-is-exa.fish
@@ -5,7 +5,7 @@ if command -q exa
     set -xg EXA_COLORS "uu=36:gu=37:sn=32:sb=32:da=34:ur=34:uw=35:ux=36:ue=36:gr=34:gw=35:gx=36:tr=34:tw=35:tx=36:"
 
     # useful aliases
-    alias ls "exa --group-directories-first --classify=auto"
+    alias ls "exa --group-directories-first --classify"
     alias ll "ls --long --hyperlink"
     alias lt "ls --tree"
 
