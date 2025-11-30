d0d01c8c7d22716d1df139ec6b88b842e0fd6386
Anton Semjonov <anton@semjonov.de>
2025-12-01 00:32:40 +0100

    fixup! fish: print a red warning for root users

--- fish/conf.d/root-user.fish
+++ fish/conf.d/root-user.fish
@@ -1,4 +1,4 @@
 # print a warning for root
-if [ (id -un) = root ]
-	echo (set_color red)" ~~ Here be dragons. This is a root shell! ~~"(set_color normal)
+if status is-interactive && [ (id -un) = root ]
+    echo (set_color red)" ~~ Here be dragons. This is a root shell! ~~"(set_color normal)
 end
