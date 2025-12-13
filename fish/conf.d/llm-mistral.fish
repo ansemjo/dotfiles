7bbc1fd4c7555a24f1750b2d9e37cd3475f0e5d5
Anton Semjonov <anton@semjonov.de>
2025-12-13 15:05:36 +0100

    fish: replace mods with llm abbreviations

---
+++ fish/conf.d/llm-mistral.fish
@@ -0,0 +1,6 @@
+# aliases for mistral models with simonw/llm
+if command -q llm
+    abbr mistral "llm -m mistral"
+    abbr codestral "llm -m codestral"
+    abbr nemo "llm -m nemo"
+end
--- fish/conf.d/mods-mistral.fish
+++
@@ -1,6 +0,0 @@
-# aliases for mistral models
-if command -q mods
-    abbr mistral "mods -m mistral-large"
-    abbr codestral "mods -m codestral"
-    abbr nemo "mods -m nemo"
-end
